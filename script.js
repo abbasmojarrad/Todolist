9bd281e5409f04024a7a3f76d53dd9a26a24fa01
abbasmojarrad <abbasmojarrad7@gmail.com>
2022-04-17 20:52:13 +0430

    change files

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const inputElm = document.querySelector(".todo-input");
-const addTodoBtn = document.querySelector(".todo-button");
-const filterTodo = document.querySelector(".filter-todo");
-const todoListContainer = document.querySelector(".todo-list");
+
+
+const $ = document;
+const inputElm = $.querySelector(".todo-input");
+const addTodoBtn = $.querySelector(".todo-button");
+const filterTodo = $.querySelector(".filter-todo");
+const todoListContainer = $.querySelector(".todo-list");
 let todoList = [];
 
 const completedFunc = (id) => {
   todoList.find((todo) => {
     todo.id === id && (todo.complete = !todo.complete);
   });
+
   filterGenerate();
   localStorage.setItem("todos", JSON.stringify(todoList));
 };
@@ -19,17 +23,20 @@ const deleteFunc = (id) => {
 const filterGenerate = () => {
   const filterMode = filterTodo.value;
   let filterTodos = null;
-  if (filterMode === "completed") {
+  if(filterMode === "completed") {
     filterTodos = todoList.filter((todo) => todo.complete);
   } else if (filterMode === "uncompleted") {
     filterTodos = todoList.filter((todo) => !todo.complete);
   } else {
     filterTodos = todoList;
   }
-
   
+
   generateTodo(filterTodos);
+ 
 };
+
+
 
 const generateTodo = (todos) => {
   todoListContainer.innerHTML = "";
@@ -37,9 +44,9 @@ const generateTodo = (todos) => {
     todoListContainer.insertAdjacentHTML(
       "beforeend",
       `<div class="todo">
-            <li class="todo-item ${todo.complete ? "completed" : ""}" data-id=${
-        todo.id
-      } >${todo.name}</li>
+            <li class="todo-item ${todo.complete?'completed' : '' }" data-id=${todo.id} >${
+        todo.name
+      }</li>
              <button class="complete-btn" onclick=completedFunc(${
                todo.id
              }) ><i class="fas fa-check"></i></button>
@@ -49,6 +56,12 @@ const generateTodo = (todos) => {
       </div>`
     );
   });
+  gsap.from(".todo", {
+    opacity: 0, 
+    y: 50, 
+    duration: .7,
+ 
+  });
 };
 
 filterTodo.addEventListener("change", filterGenerate);
@@ -56,7 +69,7 @@ addTodoBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let userVal = inputElm.value;
   let userTodo = {
-    id: Math.floor(Math.random() * 9999999 * (Math.random() * 100000)),
+    id: (Math.floor((Math.random() * 9999999)*(Math.random() * 100000))),
     name: userVal,
     complete: false,
   };
@@ -72,4 +85,23 @@ addTodoBtn.addEventListener("click", (e) => {
 window.addEventListener("load", () => {
   todoList = JSON.parse(localStorage.getItem("todos")) || [];
   filterGenerate();
+  gsap.from(".hero-text", {
+    opacity: 0, 
+    y: -100, 
+    duration: .7
+  }); 
+   gsap.from(".select", {
+    opacity: 0, 
+    x: 100, 
+    duration: .8,
+    delay:.5
+  });   
+  gsap.from(".input-container", {
+    opacity: 0, 
+    x: -100, 
+    duration: .8,
+    delay:1
+  }); 
+
+ 
 });
